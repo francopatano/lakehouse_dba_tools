d7ef9f4acc09111c42d8d63b4c0f07396247e82c
francopatano <francopatano>
2022-04-15 16:19:38 +0000

    push2

--- tasks/import_dbsql_pricing.py
+++ tasks/import_dbsql_pricing.py
@@ -1,9 +1,14 @@
 # Databricks notebook source
+import pandas as pd
 
 
 # COMMAND ----------
 
-spark.read.format("csv").option("header", "true").option("inferSchema","true").load("dbfs:/FileStore/shared_uploads/franco.patano@databricks.com/Untitled_spreadsheet___Sheet1__2_.csv").createOrReplaceTempView("dbsql_pricing")
+df = pd.read_csv("/Workspace/Repos/franco.patano@databricks.com/lakehouse_dba_tools/resources/azure_databricks_sql_pricing.csv")
+
+# COMMAND ----------
+
+spark.createDataFrame(df).createOrReplaceTempView("dbsql_pricing")
 
 # COMMAND ----------
 
@@ -12,6 +17,14 @@ spark.read.format("csv").option("header", "true").option("inferSchema","true").l
 # COMMAND ----------
 
 # MAGIC %sql create or replace table unity_information.dbsql_pricing_table as select * from dbsql_Pricing; 
+
+# COMMAND ----------
+
+# MAGIC %sql OPTIMIZE unity_information.dbsql_pricing_table
+
+# COMMAND ----------
+
+# MAGIC %sql vacuum unity_information.dbsql_pricing_table
 
 # COMMAND ----------
 
